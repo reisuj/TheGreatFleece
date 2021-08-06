1907a4ae1ddded50128da5e5c71c60713f84e3fb
REISU-LEOPARD\reisu <reisuj@gmail.com>
2021-08-06 15:14:23 -0400

    Camera trigger setup completed

---
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public Transform myCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(this.name + "Activated.");
+            Camera.main.transform.position = myCamera.transform.position;
+            Camera.main.transform.rotation = myCamera.transform.rotation;
+        }
+    }
+}
