3de81d2a0980075eedd887e93f5b859deb36f488
REISU-LEOPARD\reisu <reisuj@gmail.com>
2021-08-06 16:07:40 -0400

    Player movement system with animation complete

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -5,29 +5,37 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    private Animator _anim;
     private NavMeshAgent _agent;
+    private Vector3 _target;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if left click
         if (Input.GetMouseButtonDown(0))
         {
             Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if (Physics.Raycast(rayOrigin, out hitInfo))
             {
-                Debug.Log("Hit: " + hitInfo.point);
-                Debug.Log("Hit Name: " + hitInfo.transform.name);
-
                 _agent.SetDestination(hitInfo.point);
+                _anim.SetBool("Walk", true);
+                _target = hitInfo.point;
             }
+        }
+
+        float distance = Vector3.Distance(transform.position, _target);
+
+        if (distance < 1.0f)
+        {
+            _anim.SetBool("Walk", false);
         }
     }
 }
